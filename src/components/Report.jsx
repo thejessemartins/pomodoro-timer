ce3ab20d2fbf67daf091d17fb1f7504bb43d0c72
Jessé Martins <jessemartins.programacao@gmail.com>
2025-02-03 10:01:57 -0300

    Adicionando componente Report

---
+++ src/components/Report.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Report = ({ sessions }) => {
+  return (
+    <div className="mt-5">
+      <h3>Productivity Report</h3>
+      <ul className="list-group">
+        {sessions.map((session, index) => (
+          <li key={index} className="list-group-item">
+            {session.type}: {session.duration} minutes
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Report;
